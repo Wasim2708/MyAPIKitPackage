7643580ee3285150489ba7f68a02c977c49a34c4
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-10 10:06:00 +0530

    API Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,20 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyAPIKitPackage",
-            targets: ["MyAPIKitPackage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["ZohoDeskPortalAPIKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyAPIKitPackage",
-            dependencies: []),
-        .testTarget(
-            name: "MyAPIKitPackageTests",
-            dependencies: ["MyAPIKitPackage"]),
+        .binaryTarget(name: "ZohoDeskPortalAPIKit",
+                      url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0_BETA_1_0/ZohoDeskPortalAPIKit.zip",
+                      checksum: "396d810d248905cc3ff3c4160ecd8f528fc1a7814e28a48186dc258e463c30c8"
+        )
     ]
 )
--- README.md
+++
@@ -1,3 +0,0 @@
-# MyAPIKitPackage
-
-A description of this package.
